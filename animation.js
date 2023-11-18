282625acf1c01fafdecd66836ae67c6ff38c1c51
hieupmv <hieu0332997131@gmail.com>
2023-11-18 10:58:50 +0700

    add

---
+++ animation.js
@@ -0,0 +1,25 @@
+onload = () => {
+	const c = setTimeout(() => {
+		document.body.classList.remove("not-loaded");
+		clearTimeout(c);
+	}, 1000);
+};
+
+const textElements = document.querySelectorAll('.text');
+
+function animateText() {
+  let delay = 0;
+  textElements.forEach((textElement) => {
+    setTimeout(() => {
+      textElement.style.animation = 'fadeIn 0.5s ease-in-out forwards';
+    }, delay);
+    delay += 150;
+  });
+
+  setTimeout(() => {
+    const rocket = document.querySelector('.rocket');
+    rocket.style.animation = 'flyUp 2s ease-in-out forwards';
+  }, delay + 500);
+}
+
+animateText();
